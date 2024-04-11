8c3f36e963dd9af3363e61c107322f7170ca0be8
moonyoungseong <ansdudtjd123@naver.com>
2024-04-11 23:49:04 +0900

    클래스 주석
    아이템 클래스 주석

--- FarmLive/Assets/Script/InventoryManager.cs
+++ FarmLive/Assets/Script/InventoryManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable] // 직렬화로 인스펙터 창에서도 보이게
-public class Item
+public class Item   // 아이템 클래스
 {
     public string Type, Name, Explain, Number, Price;
     public bool isUsing;
@@ -12,7 +12,7 @@ public class Item
 public class InventoryManager : MonoBehaviour
 {
     public TextAsset ItemDatabase;  // 메모장 아이템
-    public Item MyItem;
+    public Item MyItem; // 아이템 클래스 사용
 
     void Start()
     {
