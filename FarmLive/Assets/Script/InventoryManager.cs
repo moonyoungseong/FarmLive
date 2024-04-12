693b404e3e4d434a5dba52e8f8a4de8cd5c8335e
moonyoungseong <ansdudtjd123@naver.com>
2024-04-12 15:05:26 +0900

    데이터 오류
    아이템 가져오는데 문제 생김

--- FarmLive/Assets/Script/InventoryManager.cs
+++ FarmLive/Assets/Script/InventoryManager.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 [System.Serializable] // 직렬화로 인스펙터 창에서도 보이게
 public class Item   // 아이템 클래스
 {
-    public string Type, Name, Explain, Number, Price;
+    public Item(string _Type, string _Name, string _Explain, string _Number, string _Price, bool _isUsing, string _Index)
+    { Type = _Type; Name = _Name; Explain = _Explain; Number = _Number; Price = _Price; isUsing = _isUsing; Index = _Index; }
+
+    public string Type, Name, Explain, Number, Price, Index;
     public bool isUsing;
 }
 
 public class InventoryManager : MonoBehaviour
 {
     public TextAsset ItemDatabase;  // 메모장 아이템
-    public Item MyItem; // 아이템 클래스 사용
+    public List<Item> AllItemList; // 아이템 클래스 사용
 
     void Start()
     {
-        
+        string[] line = ItemDatabase.text.Substring(0, ItemDatabase.text.Length - 1).Split('\n');
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            string[] row = line[i].Split('\t');
+            AllItemList.Add(new Item(row[0], row[1], row[2], row[3], row[4], row[5] == "TRUE", row[6]));
+        }
     }
 }
